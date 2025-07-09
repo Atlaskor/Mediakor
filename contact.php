6a8ece47d442cb9216fcfd5a5e1fca1bb3e1a38e
Atlaskor <atlaskor@mediakor.com>
2025-07-09 10:36:24 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -43,7 +43,6 @@
                     <li><a href="about.php" class="block py-2 px-3 rounded-md text-blue-600 hover:bg-blue-50 hover:text-blue-700 transition-colors duration-200">About Us</a></li>
                     <li><a href="services.php" class="block py-2 px-3 rounded-md text-blue-600 hover:bg-blue-50 hover:text-blue-700 transition-colors duration-200">Services</a></li>
                     <li><a href="contact.php" class="block py-2 px-3 rounded-md text-blue-600 hover:bg-blue-50 hover:text-blue-700 transition-colors duration-200">Contact</a></li>
-                    <li><a href="page5.php" class="block py-2 px-3 rounded-md text-blue-600 hover:bg-blue-50 hover:text-blue-700 transition-colors duration-200"></a></li>
                 </ul>
             </nav>
         </aside>
